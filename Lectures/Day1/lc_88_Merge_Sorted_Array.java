472daf52af7d7a302ea32e31c89f4be996a9d388
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-09-27 19:32:26 +0530

    Create lc_88_Merge_Sorted_Array.java

---
+++ Lectures/Day1/lc_88_Merge_Sorted_Array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] arr=new int[n+m];
+        int curr=0;
+        for(int i=0;i<m;i++){
+            arr[curr]=nums1[i];
+            curr++;
+        }
+        for(int i=0;i<n;i++){
+            arr[curr]=nums2[i];
+            curr++;
+        }
+        for(int i=0;i<m+n;i++){
+            nums1[i]=arr[i];
+        }  
+        Arrays.sort(nums1);    
+    }
+}
