945d80b228d7d0338028505dc3cbd8769e59ede3
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-09-29 21:04:46 +0530

    Create ClientTwoD.java

---
+++ Lectures/Day3/ClientTwoD.java
@@ -0,0 +1,19 @@
+
+public class ClientTwoD {
+    public static void main(String[] args) {
+        // Scanner sc = new Scanner(System.in);
+
+        // int n = sc.nextInt();
+        // int m = sc.nextInt();
+
+        // TwoD arr = new TwoD(n, m);
+        int [][] a=new int[2][];
+        System.out.println(a.length +" ");
+
+        // arr.createArr();
+        // arr.rowTraversal();
+        // arr.colTraversal();
+        // arr.diaTraversal();
+        // arr.spiralTraversal();
+    }
+}
