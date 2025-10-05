fb301e5ff78e7e80eb588a0e7fdaedee9bb69953
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-10-05 17:45:54 +0530

    Create lowerBound.java

---
+++ Lectures/Day4/lowerBound.java
@@ -0,0 +1,17 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        int lo=0;
+        int  hi=arr.length-1;
+        if(target>arr[hi]) return hi+1;
+        while(lo<hi){
+            int mid=(lo+hi)/2;
+            if(target<=arr[mid]){
+                hi=mid;
+            }
+            else{
+                lo=mid+1;
+            }
+        }
+        return lo;
+    }
+}
