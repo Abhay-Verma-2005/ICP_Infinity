c08008baba815c7ee984c6378300be031e75cacc
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-10-12 21:19:50 +0530

    Create minCost.java

---
+++ Lectures/Day8/minCost.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+      int [] dp=new int[cost.length];
+      Arrays.fill(dp,-1);
+      return Math.min(minCost(cost,0,dp),minCost(cost,1,dp));
+    }
+    public int minCost(int[] cost,int i,int[] dp){
+        if(i>=cost.length) return 0;
+        
+        if(dp[i]!=-1){
+            return dp[i];
+        }
+        int ans=cost[i]+minCost(cost,i+1,dp);
+        int ans2=cost[i]+minCost(cost,i+2,dp);
+        return dp[i]=Math.min(ans,ans2);
+    } 
+}
