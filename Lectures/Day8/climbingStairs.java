bcac5d4fbf592c03b027b38c9f3667177752f76c
ABHAY VERMA <144254558+Abhay-Verma-2005@users.noreply.github.com>
2025-10-12 21:21:42 +0530

    Create climbingStairs.java

---
+++ Lectures/Day8/climbingStairs.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 2) return n;
+        int[] dp = new int[n+1];
+        dp[2] = 2;
+        dp[1] = 1;
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public int solveStair(int n, int c, int[] dp){
+        if(n==c){
+            return 1;
+        }
+        if(n < c) return 0;
+        if(dp[c] != -1){
+            return dp[c];
+        }
+        int call1 = solveStair(n, c+1, dp);
+        int call2 = solveStair(n, c+2, dp);
+        return dp[c] = call1 + call2;
+    }
+}
